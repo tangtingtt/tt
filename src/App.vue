1ff16b2e0f22bf15949d05436410e18fbd18e6e7
ting.tang <ting.tang@chinacreator.com>
2020-06-21 22:31:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.jpg" class="index-logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.index-logo {
+  width: 200px;
+  border-radius: 50%;
+  height: 200px;
+}
+</style>
