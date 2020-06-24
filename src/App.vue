84f5ca39354276bcc7ee6670c9b224741566843b
ting.tang <ting.tang@chinacreator.com>
2020-06-24 23:44:13 +0800

    首页tab

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.inline-block {
+  display: inline-block;
+}
+.flex-block {
+  display: flex;
+}
+.max-width {
+  width: 1200px;
+}
+.margin-auto {
+  margin: 0 auto;
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="index-container">
-    这是首页
+    <div class="header-container">
+      <div class="header-container-box max-width flex-block margin-auto">
+        <div>
+          <img src="../assets/my-logo.png" class="index-logo">
+        </div>
+        <div>
+          <div v-for="(item,index) in headerList" :key="index" class="header-container-item inline-block">{{item.name}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="main-container">这是首页</div>
   </div>
 </template>
 
@@ -9,6 +19,13 @@ export default {
   name: 'Index',
   data () {
     return {
+      headerList: [
+        {name: '首页'},
+        {name: 'CSS效果'},
+        {name: 'swiprt效果'},
+        {name: '博客'},
+        {name: '关于我'}
+      ]
     }
   }
 }
@@ -16,5 +33,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header-container {
+    height: 60px;
+}
+.header-container-box {
+  justify-content: space-between;
+  align-items: center;
+}
+.header-container-item {
+  cursor: pointer;
+  color: pink;
+  margin-left: 30px;
+  padding: 10px 30px;
+  position: relative;
+}
+.header-container-item:before {
+    position: absolute;
+    right: 50%;
+    bottom: 0px;
+    width: 0;
+    height: 2px;
+    content: "";
+    -webkit-transition: width .2s ease-out;
+    transition: width .2s ease-out;
+    background: currentColor;
+}
+.header-container-item:after {
+    position: absolute;
+    bottom: 0px;
+    left: 50%;
+    width: 0;
+    height: 2px;
+    content: "";
+    -webkit-transition: width .2s ease-out;
+    transition: width .2s ease-out;
+    background: currentColor;
+}
+.header-container-item:hover:after, .header-container-item:hover:before {
+  width: 20%;
+}
+.main-container {
+  background-color: pink;
+}
 </style>
